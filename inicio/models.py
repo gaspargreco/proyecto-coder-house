799c0b9a60e94b35d6428f7c4c8b107fe5b62b5d
gaspiigreco <gaspargreco13@gmail.com>
2026-04-01 21:09:19 -0300

    creacion de aplicacion inicio

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class DepartamentosMedicos(models.Model):
+    nombre = models.CharField(max_length=50)
+    nro_departamento = models.IntegerField(unique=True)
+    nro_medicos = models.IntegerField(null=True)
+    fecha = models.DateField(auto_now_add=True)
+    email_departemento = models.EmailField()
+    
+    def __str__(self):
+        return f"Departamento: {self.nombre} - Nro: {self.nro_departamento}"
+
+
