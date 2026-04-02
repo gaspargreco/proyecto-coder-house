dd22b2dd49877348ce85096f65b1b00c1a6c64b1
gaspiigreco <gaspargreco13@gmail.com>
2026-04-01 21:15:19 -0300

    "commit"

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from inicio.models import DepartamentosMedicos
 # Register your models here.
+
+admin.site.register(DepartamentosMedicos)
+
+@admin.register(DepartamentosMedicos)
+class DepartamentosMedicosaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "nro_departamento", "nro_medicos")
+    
+    list_display_links = ("nombre",)
+    
+    search_fields = ("nro_departamento", )
+    
+    list_filter = ("fecha", )
+    
+    ordering = ("nro_departamento", )
+    
+    readonly_fields = ("fecha", )
+    
+    
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Proyecto
+
+Explicar el proyecto, como iniciarlo y que se necesita para ejecutarlo
+
+
+# se ignora siempre al principio para no olvidar
